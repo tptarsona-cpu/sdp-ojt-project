9964d447c4b272dbf5861cb261001d42f6e8c048
tptarsona-cpu <tptarsona@ccc.edu.ph>
2026-01-06 09:06:43 +0800

    change

--- sdp-ojt-project/Login.php
+++ sdp-ojt-project/Login.php
@@ -7,7 +7,7 @@ $demo_email = 'test@example.com';
 $demo_password = 'password';
 
 // Logout
-if if(isset($_GET['logout'])) {
+if (isset($_GET['logout'])) {
     session_destroy();
     header("Location: Login.php");
     exit();
